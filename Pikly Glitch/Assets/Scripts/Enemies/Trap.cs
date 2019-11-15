9b47b53c537bd01425ce7be0bcd98141b5080b08
Jess <jess.thomason@outlook.com>
2019-11-15 15:47:06 +0000

    Created Generator 1 room for Central Power Systems, and implemented first version of traps

---
+++ Pikly Glitch/Assets/Scripts/Enemies/Trap.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform spawnPos;
+    [Range(1, 10)]
+    public float distance = 5;
+    [Range(1, 40)]
+    public int quantity = 10;
+    [Range(0.1f, 0.75f)]
+    public float spawnSpeed = 0.3f;
+    [Range(0.5f, 2f)]
+    public float triggerTimeMin = 0.5f, triggerTimeMax = 2f;
+
+    float insideTime, triggerTime;
+    bool setInsideTime;
+
+    public bool triggered = false;
+
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        triggerTime = Random.Range(triggerTimeMin, triggerTimeMax);
+    }
+
+    void Update() {
+        if (!player)
+            return;
+
+        if (PlayerInside()) {
+            StartCoroutine(Spawn());
+        }
+    }
+
+    bool PlayerInside() {
+        if (triggered)
+            return false;
+
+        if (Vector2.Distance(transform.position, player.transform.position) < distance) {
+            if (!setInsideTime) {
+                insideTime = Time.time;
+            }
+
+            if (Time.time < triggerTime + insideTime) {
+                triggered = true;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    IEnumerator Spawn() {
+        for(int i = 0; i < quantity; i++) {
+            GameObject.Instantiate(enemy, spawnPos.position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnSpeed);
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, distance);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(spawnPos.position, 0.125f);
+        Gizmos.color = Color.white;
+    }
+}
