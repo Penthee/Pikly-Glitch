4307f47e9dbc6724cf285e80c62e756eff80846a
Jess <Pikl@users.noreply.github.com>
2020-01-11 15:21:53 +0000

    Finishing touches on void stuff

--- Pikly Glitch/Assets/Scripts/Player/States/Evade.cs
+++ Pikly Glitch/Assets/Scripts/Player/States/Evade.cs
@@ -22,9 +22,6 @@ namespace Pikl.Player.States {
         internal override void Enter(StateObject so) {
             base.Enter(so);
 
-            //player.shipAR.SetBool("roll", true);
-            //player.shipAR.SetInteger("index", Random.Range(0, 2));
-
             evadeDir = player.input.MoveAxisRaw.normalized;
 
             if (evadeDir.magnitude == 0)
@@ -35,26 +32,15 @@ namespace Pikl.Player.States {
             player.evade.lastTime = Time.time;
             player.evade.Stamina = Mathf.Clamp(player.evade.Stamina - player.evade.EvadeCost, 0, player.evade.MaxStamina);
 
-            //var script = Camera.main.GetComponent<Utils.Effects.ImageEffects.VignetteAndChromaticAberration>();
-            //DOTween.To(() => script.chromaticAberration, x => script.chromaticAberration = x, 20, player.evade.Cooldown * 0.5f)
-                   //.OnComplete(() => DOTween.To(() => script.chromaticAberration, x => script.chromaticAberration = x, 0, player.evade.Cooldown * 0.5f));
-
             Physics2D.IgnoreLayerCollision(player.gameObject.layer, LayerMask.NameToLayer("EnemyDamage"), true);
             Physics2D.IgnoreLayerCollision(player.gameObject.layer, LayerMask.NameToLayer("Enemy"), true);
 
-            //Shaker.I.ShakeCameraOnce(ShakePresets.Evade);
             //AudioMgr.I.PlaySound(boostSound);
-
-            //player.evade.particles.transform.rotation = Quaternion.AngleAxis(Mathf.Repeat((player.input.MoveAxisRaw).ToAngle() + 120, 360), Vector3.forward);
-            //player.evade.particles.Play();
-
-            //WaveExploPostProcessing.Get().StartIt(Camera.main.WorldToViewportPoint(player.t.position), -0.15f, 1.1f, 0.75f, 0.2f, 0.025f, Ease.Linear, 0.5f, Ease.Linear);
 
         }
 
 
         internal override void Exit() {
-            //player.shipAR.SetBool("roll", false);
 
             player.evadeID = 0;
 
--- Pikly Glitch/Assets/Scripts/Void.cs
+++ Pikly Glitch/Assets/Scripts/Void.cs
@@ -13,21 +13,24 @@ public class Void : MonoBehaviour {
         Ease.InOutCirc
     };
 
-    public Vector3[] points, startingPoints;
+    public Vector3[] points, abPoints, startingPoints;
     public int[] index;
 
     [Range(0.01f, 2f)]
     public float moveVariation = 1.1f, minDuration = 0.1f, maxDuration = 0.5f;
 
-    public Material material;
+    public Material material, red, yellow, green;
+    Material abMaterial;
 
     GameObject voidObj;
 
     PolygonCollider2D poly;
-    MeshRenderer meshRenderer;
-    MeshFilter meshFilter;
+    MeshRenderer meshRenderer, abRenderer;
+    MeshFilter meshFilter, abFilter;
 
-    public Vector3[] variation;
+    Transform player;
+
+    public Vector3[] variation, abVariation;
 
     public Ease GetEase() {
         return eases[Random.Range(0, eases.Length - 1)];
@@ -41,22 +44,36 @@ public class Void : MonoBehaviour {
         GetComponents();
 
         startingPoints = (Vector3[])points.Clone();
+        abPoints = (Vector3[])points.Clone();
+
+        switch (Random.Range(0, 3)) {
+            case 0: abMaterial = red; break;
+            case 1: abMaterial = yellow; break;
+            case 2: abMaterial = green; break;
+        }
 
         DoTheThing();
     }
 
     void GetComponents() {
+        player = GameObject.Find("Player").transform;
+
         meshRenderer = GetComponent<MeshRenderer>();
         meshFilter = GetComponent<MeshFilter>();
         poly = GetComponent<PolygonCollider2D>();
+        abRenderer = transform.GetChild(0).GetComponent<MeshRenderer>();
+        abFilter= transform.GetChild(0).GetComponent<MeshFilter>();
+
     }
 
     int i = 0;
     void CalcVariations() {
         variation = new Vector3[points.Length];
-        
+        abVariation = new Vector3[points.Length];
+
         for (i = 0; i < points.Length; i++) {
             variation[i] = startingPoints[i] + new Vector3(Random.Range(-moveVariation, moveVariation), Random.Range(-moveVariation, moveVariation), 0);
+            abVariation[i] = startingPoints[i] + new Vector3(Random.Range(-moveVariation, moveVariation), Random.Range(-moveVariation, moveVariation), 0);
         }
     }
 
@@ -67,11 +84,22 @@ public class Void : MonoBehaviour {
         DOTween.To(() => points[0], x => points[0] = x, variation[0], duration).SetEase(GetEase());
         DOTween.To(() => points[1], x => points[1] = x, variation[1], duration).SetEase(GetEase());
         DOTween.To(() => points[2], x => points[2] = x, variation[2], duration).SetEase(GetEase()).OnComplete(DoTheThing);
+
+        DOTween.To(() => abPoints[0], x => abPoints[0] = x, abVariation[0], duration).SetEase(GetEase());
+        DOTween.To(() => abPoints[1], x => abPoints[1] = x, abVariation[1], duration).SetEase(GetEase());
+        DOTween.To(() => abPoints[2], x => abPoints[2] = x, abVariation[2], duration).SetEase(GetEase());
     }
 
     void Update() {
+#if UNITY_EDITOR
+        if (player != null && Vector2.Distance(player.position, transform.position) > 50)
+            return;
+#endif 
         meshRenderer.material = material;
-        meshFilter.mesh = CreateMesh();
+        meshFilter.mesh = CreateMesh(points);
+
+        abRenderer.material = abMaterial;
+        abFilter.mesh = CreateMesh(abPoints);
 
         UpdatePolygon();
     }
@@ -81,12 +109,12 @@ public class Void : MonoBehaviour {
         poly.SetPath(0, new Vector2[] { points[0], points[1], points[2] });
     }
 
-    Mesh CreateMesh() {
+    Mesh CreateMesh(Vector3[] _points) {
         Mesh mesh = new Mesh();
-        var vertex = new Vector3[points.Length];
+        var vertex = new Vector3[_points.Length];
 
-        for (int x = 0; x < points.Length; x++) {
-            vertex[x] = points[x];
+        for (int x = 0; x < _points.Length; x++) {
+            vertex[x] = _points[x];
         }
 
         var uvs = new Vector2[vertex.Length];
@@ -131,5 +159,6 @@ public class Void : MonoBehaviour {
     void OnDrawGizmos() {
         GetComponents();
         UpdatePolygon();
+        
     }
 }
